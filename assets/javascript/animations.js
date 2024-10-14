cd7be676e3e3e79393db214ba4190e1a5ecde2cf
Marion <marion.carambula@modop.com>
2024-10-14 07:48:05 -0500

    fixing screenshots

--- assets/javascript/animations.js
+++ assets/javascript/animations.js
@@ -6,17 +6,38 @@ var animationTimeline = function () {
   // Animation timeline
   const tl = gsap.timeline();
 
-  tl.from(".thumbnail", {
+  tl.from(".profile-thumbnail", {
     duration: 0.5,
     scale: 0.5,
     opacity: 0,
     ease: "power3.in",
   })
-    .from(".name", { duration: 0.25, y: 10, opacity: 0, ease: "power3.in" })
-    .from(".location", { duration: 0.25, y: 10, opacity: 0, ease: "power3.in" })
-    .from(".description", { duration: 0.25, y: 10, opacity: 0, ease: "power3.in" })
+    .from(".profile-name", {
+      duration: 0.25,
+      y: 10,
+      opacity: 0,
+      ease: "power3.in",
+    })
+    .from(".profile-location", {
+      duration: 0.25,
+      y: 10,
+      opacity: 0,
+      ease: "power3.in",
+    })
+    .from(".profile-description", {
+      duration: 0.25,
+      y: 10,
+      opacity: 0,
+      ease: "power3.in",
+    })
     .from(".socials-links", { duration: 0.15, opacity: 0, ease: "power3.in" })
-    .from(".socials-link", { duration: 0.15, y: 10, opacity: 0, ease: "power3.in", stagger: 0.1 });
+    .from(".socials-link", {
+      duration: 0.15,
+      y: 10,
+      opacity: 0,
+      ease: "power3.in",
+      stagger: 0.1,
+    });
 };
 
 window.onload = function (e) {
